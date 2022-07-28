e8f951e744495a97f85b2b0b5d5344bd0396d724
Ahmed Tanvir <mahmedexec@gmail.com>
2022-07-28 19:16:35 +0500

    feat: complete

---
+++ vue-node-worker/src/index.ts
@@ -0,0 +1,22 @@
+export interface Env {
+  API_URL_DEV: string;
+  API_URL_TEST: string;
+  API_URL_STAGING: string;
+  API_URL_PRODUCTION: string;
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const data = await (await fetch(env.API_URL_DEV)).json();
+
+    console.log(request.url, ctx);
+
+    return new Response(JSON.stringify(data), {
+      headers: { "content-type": "application/json;charset=UTF-8" },
+    });
+  },
+};
