fc22a2843fe8b9101d394c69e480446ef17314ff
Ahmed Tanvir <mahmedexec@gmail.com>
2022-07-28 22:55:27 +0500

    feat: all current state of code

--- vue-node-worker/src/index.ts
+++ vue-node-worker/src/index.ts
@@ -1,34 +1,38 @@
-export interface Env {
-  API_URL_DEV: string;
-  API_URL_TEST: string;
-  API_URL_STAGING: string;
-  API_URL_PRODUCTION: string;
-  PRODUCTION_URL: string;
-  DEV_URL: string;
-  TESTING_URL: string;
-  STAGING_URL: string;
-}
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 
-export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext
-  ): Promise<Response> {
-    let API_URL;
+async function handleRequest(request: Request): Promise<Response> {
+  const env = {
+    API_URL_DEV: "https://rickandmortyapi.com/api/character/61",
+    API_URL_TEST: "https://rickandmortyapi.com/api/character/281",
+    API_URL_STAGING: "https://rickandmortyapi.com/api/character/628",
+    API_URL_PRODUCTION: "https://rickandmortyapi.com/api/character/773",
+    PRODUCTION_URL: "https://vue-node.pages.dev/",
+    DEV_URL: "https://dev.vue-node.pages.dev/",
+    TESTING_URL: "https://testing.vue-node.pages.dev/",
+    STAGING_URL: "https://staging.vue-node.pages.dev/",
+  };
+  let API_URL;
 
-    if (request.url === env.PRODUCTION_URL) API_URL = env.API_URL_PRODUCTION;
-    else if (request.url === env.STAGING_URL) API_URL = env.API_URL_STAGING;
-    else if (request.url === env.TESTING_URL) API_URL = env.API_URL_TEST;
-    else API_URL = env.API_URL_DEV;
+  if (request.url === env.PRODUCTION_URL) API_URL = env.API_URL_PRODUCTION;
+  else if (request.url === env.STAGING_URL) API_URL = env.API_URL_STAGING;
+  else if (request.url === env.TESTING_URL) API_URL = env.API_URL_TEST;
+  else API_URL = env.API_URL_DEV;
 
-    const data = await (await fetch(API_URL)).json();
+  const data = await (await fetch(API_URL)).json();
 
-    return new Response(JSON.stringify(data), {
+  return new Response(
+    JSON.stringify({
+      ...(data as any),
+      url: request.url,
+      headers: request.headers,
+    }),
+    {
       headers: {
         "content-type": "application/json;charset=UTF-8",
         "Access-Control-Allow-Origin": "*",
       },
-    });
-  },
-};
+    }
+  );
+}
